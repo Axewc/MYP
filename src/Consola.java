9aff44203e0d931242faf3da65b1160d61f46f4f
EliotFigueroa <41647303+EliotFigueroa@users.noreply.github.com>
2020-03-07 18:07:34 -0600

    Intento de simulacion

--- src/Consola.java
+++ src/Consola.java
@@ -2,57 +2,127 @@
 import java.util.LinkedList;
 
 public class Consola {
-    // Servicio netflix = new Servicio();
-    static Servicio ne = new Servicio();
-    static Servicio yt = new Servicio();
-    
-    public static void main(String[] args) {
-	Persona bob = new Persona(1, 50.00, "Bob");
-	Persona Alicia = new Persona(1, 500.00, "Alicia");
-	Persona Cesar = new Persona(10, 40.00, "Cesar");
-	//Bob
-	ne.netflix();
-	yt.youtube();
-	
-	ne.getServ().pago(bob.getTipoSusc(), bob, ne);
-	System.out.println(bob.getPresupuesto());
-	ne.getServ().pago(bob.getTipoSusc(), bob, ne);
-	System.out.println(bob.getPresupuesto());
-	ne.getServ().cancelaSuscripcion(bob, ne);
-	System.out.println("");
 
-	//Alicia
+    static Servicio ne = new Servicio(); //1,2,3
+    static Servicio am = new Servicio(); //4,5
+    static Servicio sp = new Servicio(); //6,7
+    static Servicio tw = new Servicio(); //8 
+    static Servicio yt = new Servicio(); //9,10
+    
+    
+    public static void main(String[] args) { 	
+	Persona Alicia = new Persona(3, 500.00, "Alicia");
+	Persona Bob = new Persona(3, 40.00, "Bob");
+	Persona Cesar = new Persona(7, 40.00, "Cesar");
+        Persona Diego = new Persona(2, 180.00, "Diego");
+	Persona Erika = new Persona(3, 300.00, "Erika");
+
+	ne.netflix();
+	am.amazon();
+        sp.spotify();
+	tw.twitch();
+	yt.youtube();
+
+	//Dia 1
+	System.out.println("Primer dia de cobros");
+	//Netflix
 	ne.getServ().pago(Alicia.getTipoSusc(), Alicia, ne);
+	/*
+	ne.getServ().pago(Bob.getTipoSusc(), Bob, ne);
+	
+	ne.getServ().pago(Diego.getTipoSusc(), Diego, ne);
+	
+	ne.getServ().pago(Erika.getTipoSusc(), Erika, ne);
+	*/
+	//Amazon
+	Alicia.setTipoSusc(5);
+	am.getServ().pago(Alicia.getTipoSusc(), Alicia, am);
+	/*
+	Bob.setTipoSusc(5);
+	am.getServ().pago(Bob.getTipoSusc(), Bob, am);
+
+	Diego.setTipoSusc(5);
+	am.getServ().pago(Diego.getTipoSusc(), Diego, am);
+	*/
+	//Spotify
+	Alicia.setTipoSusc(7);
+	sp.getServ().pago(Alicia.getTipoSusc(), Alicia, sp);
+	/*
+	Bob.setTipoSusc(7);
+	sp.getServ().pago(Bob.getTipoSusc(), Bob, sp);
+	
+	sp.getServ().pago(Cesar.getTipoSusc(), Cesar, sp);	
+
+	Erika.setTipoSusc(6);
+	sp.getServ().pago(Erika.getTipoSusc(), Erika, sp);
+	*/
+	//Twitch
+	Alicia.setTipoSusc(8);
+	tw.getServ().pago(Alicia.getTipoSusc(), Alicia, tw);
+	/*
+	Bob.setTipoSusc(8);
+	tw.getServ().pago(Bob.getTipoSusc(), Bob, tw);
+
+	Erika.setTipoSusc(8);
+	tw.getServ().pago(Erika.getTipoSusc(), Erika, tw);
+	*/
+	//Youtube
+	Alicia.setTipoSusc(10);
+	yt.getServ().pago(Alicia.getTipoSusc(), Alicia, yt);
 	System.out.println(Alicia.getPresupuesto());
+	/*
+	Bob.setTipoSusc(10);
+	yt.getServ().pago(Bob.getTipoSusc(), Bob, yt);
+
+	Cesar.setTipoSusc(9);
+	yt.getServ().pago(Cesar.getTipoSusc(), Cesar, yt);
+	
+	Erika.setTipoSusc(9);
+	yt.getServ().pago(Erika.getTipoSusc(), Erika, yt);
+	*/
+	
+	//Día 2
+	System.out.println("Segundo dia de cobros");
+	//Netflix
+	Alicia.setTipoSusc(3);
 	ne.getServ().pago(Alicia.getTipoSusc(), Alicia, ne);
-	System.out.println(Alicia.getPresupuesto());
-	ne.getServ().cancelaSuscripcion(Alicia, ne);
-	ne.getServ().pago(Alicia.getTipoSusc(), Alicia, ne);
-	System.out.println(Alicia.getPresupuesto());	
-	Alicia.setTipoSusc(2);
-	yt.getServ().pago(Alicia.getTipoSusc(), Alicia, ne);
-	System.out.println(Alicia.getPresupuesto());
-	yt.getServ().pago(Alicia.getTipoSusc(), Alicia, ne);
-	System.out.println(Alicia.getPresupuesto());
-	System.out.println("");
+
+	//Bob.setTipoSusc(3);
+	//ne.getServ().pago(Bob.getTipoSusc(), Bob, ne);
+
+	//Amazon
+	Alicia.setTipoSusc(5);
+	am.getServ().pago(Alicia.getTipoSusc(), Alicia, am);
+
+	//Bob.setTipoSusc(5);
+	//am.getServ().pago(Bob.getTipoSusc(), Bob, am);
 	
-	//Cesar
-	yt.getServ().pago(Cesar.getTipoSusc(), Cesar, yt);
-	yt.getServ().pago(Cesar.getTipoSusc(), Cesar, yt);
-        yt.getServ().cancelaSuscripcion(Cesar, yt);
-	Cesar.setTipoSusc(11);
-	yt.getServ().pago(Cesar.getTipoSusc(), Cesar, yt);
-	System.out.println(Cesar.getPresupuesto());
+	//Spotify
+	Alicia.setTipoSusc(7);
+	sp.getServ().pago(Alicia.getTipoSusc(), Alicia, sp);
+
+	//Bob.setTipoSusc(7);
+	//sp.getServ().pago(Bob.getTipoSusc(), Bob, sp);
+
+	//Cesar.setTipoSusc(7);
+	//sp.getServ().pago(Cesar.getTipoSusc(), Cesar, sp);
 	
-	yt.getServ().pago(Cesar.getTipoSusc(), Cesar, yt);
-	System.out.println(Cesar.getPresupuesto());
-	//primer dia de netflix
+	//Twitch
+	Alicia.setTipoSusc(8);
+	tw.getServ().pago(Alicia.getTipoSusc(), Alicia, tw);
+
+	//Bob.setTipoSusc(8);
+	//tw.getServ().pago(Bob.getTipoSusc(), Bob, tw);
 	
-	/*ne.getServ().pago(Cesar.getTipoSusc(), Cesar, ne);
-	System.out.println(Cesar.getPresupuesto());
-	//segundo dia de netflix
-	ne.getServ().pago(Cesar.getTipoSusc(), Cesar, ne);
-	System.out.println(Cesar.getPresupuesto());
-	//problema al cobrar*/ 
+	//Youtube
+	Alicia.setTipoSusc(10);
+	yt.getServ().pago(Alicia.getTipoSusc(), Alicia, yt);
+	System.out.println(Alicia.getPresupuesto());
+	
+	//Bob.setTipoSusc(10);
+	//yt.getServ().pago(Bob.getTipoSusc(), Bob, yt);
+
+	//Cesar.setTipoSusc(9);
+	//yt.getServ().pago(Cesar.getTipoSusc(), Cesar, yt); 
   }
 }
