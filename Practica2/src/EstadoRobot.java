2282eb245aaeeb1e7e0955e6db92a50606c5aa2a
Axel <41756618+Axewc@users.noreply.github.com>
2020-03-14 13:07:34 -0600

    Add files via upload

---
+++ Practica2/src/EstadoRobot.java
@@ -0,0 +1,8 @@
+
+public interface EstadoRoot {
+  void suspender();
+  void recibirOrden();
+  void caminar();
+  void reabastecer();
+  void trabajar();
+}
---
+++ Practica2/src/Robot.java
@@ -0,0 +1,18 @@
+
+/*
+* Clase que va a implemnetar todas las funcionalidades según el estado del
+* Robot
+*/
+public class Robot {
+
+  EstadoRobot suspendido;
+  EstadoRobot recibiendoOrden;
+  EstadoRobot caminando;
+  EstadoRobot reabasteciendo;
+  EstadoRobot trabajando;
+  // Alguna variable que guarde la orden
+
+  public Robot() {
+
+  }
+}
